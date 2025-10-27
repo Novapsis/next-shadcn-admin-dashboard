de816c7ed63d6577a2acec627b07873b124cf75d
novapsis <novapsiscorp@gmail.com>
2025-10-27 10:18:39 +0100

    feat: set crm as default dashboard and fix redirects

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   reactCompiler: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/dashboard',
+        destination: '/dashboard/crm',
+      },
+    ]
   }
 }
 
